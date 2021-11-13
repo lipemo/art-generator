0a7959c09fbeb263bb906799526a20218afe99b3
lipemo <unconfigured@null.spigotmc.org>
2021-11-13 10:56:21 -0300

    It adds time field

--- src/arts/featbf.tsx
+++ src/arts/featbf.tsx
@@ -3,6 +3,7 @@ import dafitiFeatImg from '../assets/dafitiFeat.png';
 
 const FeatBF: React.FC<any> = React.forwardRef((props, ref:any) => {
   const [fullname, setFullname]:any = useState('');
+  const [time, setTime]:any = useState('');
   const [img, setImg]:any = useState(null);
   
   return(
@@ -10,16 +11,19 @@ const FeatBF: React.FC<any> = React.forwardRef((props, ref:any) => {
       <section ref={ref} className="sArt">
     { !props.isSaving ? 
     <form>
-      <textarea required onChange={(event:any) => { setFullname(event.target.value);}}></textarea>
+      <textarea spellCheck="false" required className="nameTextarea" onChange={(event:any) => { setFullname(event.target.value);}}></textarea>
       <input required placeholder="image" type="file" onChange={(event:any) => {setImg(()=> URL.createObjectURL(event.target.files[0]));}}/>
+      <textarea spellCheck="false" required className="timeTextarea" onChange={(event:any) => { setTime(event.target.value);}}></textarea>
       <div className="dashedAvatar"></div>
       <div className="nameTip">Digite o seu nome</div>
       <div className="avatarTip">Escolha sua imagem</div>
+      <div className="timeTip">Quanto tempo? (Ex: 5 anos)</div>
     </form> : ''
   }
         <img className="avatar" src={img} alt="Avatar"/>
         <div>{props.fullname}</div>
         <div className="fullName">{fullname}</div>
+        <div className="feat">Eu dei feat com a DFT há {time}</div>
         <img src={dafitiFeatImg} alt="Dafiti Black Friday" className="art" />
       </section>
     </>
