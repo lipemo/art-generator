bed64eba50910edbff742fab39dc71e7ff2a84fa
lipemo <unconfigured@null.spigotmc.org>
2021-11-13 05:17:12 -0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import FeatBF from './arts/featbf';
+
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from 'react-component-export-image';
+
 import './App.css';
 
 function App() {
+
+  const myRef = React.createRef();
+
   return (
-    <div className="App">
+    <div className="App">     
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
